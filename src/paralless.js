305f3f52beda164d817bc4173e04dd572fc8a490
estrattonbailey <ericstrattonbailey@gmail.com>
2016-09-25 16:24:15 -0400

    Add rAF, run through browserify standalone es2015 preset

--- src/paralless.js
+++ src/paralless.js
@@ -1,30 +1,28 @@
+export default (className) => {
+  let y
+  let tick = false
+  let targets = [].slice.call(document.querySelectorAll(className))
 
-let Paralless = (className) => {
   const instance = Object.create({
     init: () => {
-      window.addEventListener('scroll', handleScroll)
+      window.addEventListener('scroll', handler)
     }
   })
+
   return instance
 
-  function handleScroll() {
-    let elms = document.querySelectorAll(className)
-    elms.length ? Array.from(elms, (e) => { mI(e)}) : mI(elms)
-  }
+  function handler(force = false) {
+    y = window.pageYOffset 
 
-  function mI(el) {
-    let scrollPosition = setPosition()
-    let displace = el.getAttribute('data-speed') || 2
-    el.style.transform = "translate3d(0px, "+(scrollPosition / displace)+"px, 0px)"
-  }
-
-  function setPosition() {
-    if (window.pageYOffset !== undefined) {
-      return window.pageYOffset
-    } else {
-      return (document.documentElement || document.body.parentNode || document.body).scrollTop
+    if (!tick) {
+      window.requestAnimationFrame(() => targets.forEach(position))
+      tick = true
     }
   }
-}
 
-exports.Paralless = Paralless
+  function position(el) {
+    let displace = el.getAttribute('data-speed') || 2
+    el.style.transform = `translate3d(0px, ${y / displace}px, 0px)`
+    tick = false
+  }
+}
