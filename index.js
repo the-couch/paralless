50ea3cc29e4ff5070052d1265b4f4bd4352fe66b
estrattonbailey <ericstrattonbailey@gmail.com>
2018-09-24 18:50:58 -0400

    .

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
+import srraf from 'srraf'
+
 export default function paralless (attr = 'data-paralless') {
   let y = window.scrollY
-  let py = y
   let vh = window.innerHeight
   let cache = new Map()
 
   return function init () {
-    let stopped = false
-
     /**
      * remove old nodes
      */
@@ -23,56 +22,33 @@ export default function paralless (attr = 'data-paralless') {
       if (!cache.has(node)) {
         const speed = parseFloat(node.getAttribute(attr) || 0.25)
 
-        if (speed >= 5) continue
+        if (speed >= 1) continue
 
         cache.set(node, {
           node,
           speed,
-          transform: 0
+          transform: 0,
+          offset: Math.max(y + (node.getBoundingClientRect().top - vh), 0) // cache
         })
       }
     }
 
-    /**
-     * check initial pos
-     */
-    reflow()
-
-    function animate () {
-      y = window.scrollY
-
-      if (py !== y) {
-        py = y
-
-        cache.forEach(c => {
-          const { node, speed, offset } = c
-          const bounds = node.getBoundingClientRect()
-          const nodeTop = bounds.top + y
-          const nodeBot = nodeTop + bounds.height
-
-          if ((nodeBot >= y) && (nodeTop <= (y + vh))) {
-            c.transform = ((y - offset) * speed).toFixed()
-            node.style.transform = `translate3d(0px, ${c.transform}px, 0px)`
-          }
-        })
-      }
-
-      !stopped && requestAnimationFrame(animate)
-    }
-
-    function reflow () {
-      y = window.scrollY
-      vh = window.innerHeight
-
+    const listener = srraf(({ y, vh }) => {
       cache.forEach(c => {
-        c.offset = y + (c.node.getBoundingClientRect().top - vh) - c.transform
-      })
+        const { node, speed, transform, offset } = c
+        const bounds = node.getBoundingClientRect()
+        const nodeTop = bounds.top + y
+        const nodeBot = nodeTop + bounds.height
 
-      animate()
-    }
+        if ((nodeBot >= y) && (nodeTop <= (y + vh))) {
+          c.transform = Math.round(((y - offset) * speed))
+          node.style.transform = `translate3d(0px, ${c.transform}px, 0px)`
+        }
+      })
+    })
 
     return function destroy () {
-      stopped = true
+      listener()
       cache.clear()
     }
   }
